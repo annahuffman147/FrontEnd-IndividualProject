8aad60360f168f549b7b203ab802fd10c8d85ff8
annahuffman147 <annahuffman.plc@gmail.com>
2015-11-25 12:50:14 -0700

    save changes copy and paste but do not merge

---
+++ public/js/imageVariables.js
@@ -0,0 +1,347 @@
+
+var Conditions = [
+  {codeValue: "0",
+    description: "tornado",
+    imageSource: "./images/tornado.svg",
+    imageTag: '<img src="./images/tornado.svg" alt="Tornado">'
+  },
+  {codeValue: "1",
+    description: "tropical storm",
+    imageSource: '',
+    imageTag: 'return "Tropical Storm"'
+  },
+  {codeValue: "2",
+    description: "hurricane",
+    imageSource: '',
+    imageTag: 'return "Hurricane"'
+  },
+  {codeValue: "3",
+    description: "severe thunderstorms",
+    imageSource: "./images/thunderstorm.svg",
+    imageTag: '<img src="./images/thunderstorm.svg" alt="Severe Thunderstorm">'
+  },
+  {codeValue: "4",
+    description: "thunderstorms",
+    imageSource: "./images/thunderstorm.svg",
+    imageTag: '<img src="./images/thunderstorm.svg" alt="Thunderstorm">'
+  },
+  {codeValue: "5",
+    description: "mixed rain and snow",
+    imageSource: "./images/rainAndSnow.svg",
+    imageTag: '<img src="./images/rainAndSnow.svg" alt="Rain and Snow">'
+  },
+  {codeValue: "6",
+    description: "mixed rain and sleet",
+    imageSource: "./images/rainAndSleet.svg",
+    imageTag: '<img src="./images/rainAndSleet.svg" alt="Rain and Sleet">'
+  },
+  {codeValue: "7",
+    description: "mixed snow and sleet",
+    imageSource: "./images/snowAndSleet.svg",
+    imageTag: '<img src="./images/snowAndSleet.svg" alt="Snow and Sleet">'
+  },
+  {codeValue: "8",
+    description: "freezing drizzle",
+    imageSource: "./images/rainAndSleet.svg",
+    imageTag: '<img src="./images/rainAndSleet.svg" alt="Freezing Drizzle">'
+  },
+  {codeValue: "9",
+    description: "drizzle",
+    imageSource: "./images/rain.svg",
+    imageTag: '<img src="./images/rain.svg" alt="Drizzle">'
+  },
+  {codeValue: "10",
+    description: "freezing rain",
+    imageSource: "./images/rainAndSleet.svg",
+    imageTag: '<img src="./images/rainAndSleet.svg" alt="Freezing Rain">'
+  },
+  {codeValue: "11",
+    description: "showers",
+    imageSource: "./images/rain.svg",
+    imageTag: '<img src="./images/rain.svg" alt="Showers">'
+  },
+  {codeValue: "12",
+    description: "showers",
+    imageSource: "./images/rain.svg",
+    imageTag: '<img src="./images/rain.svg" alt="Showers">'
+  },
+  {codeValue: "13",
+    description: "snow flurries",
+    imageSource: "./images/snow.svg",
+    imageTag: '<img src="./images/snow.svg" alt="Snow Flurries">'
+  },
+  {codeValue: "14",
+    description: "light snow showers",
+    imageSource: "./images/snow.svg",
+    imageTag: '<img src="./images/snow.svg" alt="Light Snow Showers">'
+  },
+  {codeValue: "15",
+    description: "blowing snow",
+    imageSource: "./images/snow.svg",
+    imageTag: '<img src="./images/snow.svg" alt="Blowing Snow">'
+  },
+  {codeValue: "16",
+    description: "snow",
+    imageSource: "./images/snow.svg",
+    imageTag: '<img src="./images/snow.svg" alt="Snow">'
+  },
+  {codeValue: "17",
+    description: "hail",
+    imageSource: "./images/hail.svg",
+    imageTag: '<img src="./images/hail.svg" alt="Hail">'
+  },
+  {codeValue: "18",
+    description: "sleet",
+    imageSource: "./images/sleet.svg",
+    imageTag: '<img src="./images/sleet.svg" alt="Sleet">'
+  },
+  {codeValue: "19",
+    description: "dust",
+    imageSource: "",
+    imageTag: 'return "Dust"'
+  },
+  {codeValue: "20",
+    description: "foggy",
+    imageSource: "",
+    imageTag: 'return "Foggy"'
+  },
+  {codeValue: "21",
+    description: "haze",
+    imageSource: "",
+    imageTag: 'return "Haze"'
+  },
+  {codeValue: "22",
+    description: "smoky",
+    imageSource: "",
+    imageTag: 'return "Smoky"'
+  },
+  {codeValue: "23",
+    description: "blustery",
+    imageSource: "./images/windy.svg",
+    imageTag: '<img src="./images/windy.svg" alt="Blustery">'
+  },
+  {codeValue: "24",
+    description: "windy",
+    imageSource: "./images/windy.svg",
+    imageTag: '<img src="./images/windy.svg" alt="Windy">'
+  },
+  {codeValue: "25",
+    description: "cold",
+    imageSource: "./images/cold.svg",
+    imageTag: '<img src="./images/cold.svg" alt="Cold">'
+  },
+  {codeValue: "26",
+    description: "cloudy",
+    imageSource: "./images/cloudy.svg",
+    imageTag: '<img src="./images/cloudy.svg" alt="Cloudy">'
+  },
+  {codeValue: "27",
+    description: "mostly cloudy",
+    imageSource: "./images/mostlyCloudy.svg",
+    imageTag: '<img src="./images/mostlyCloudy.svg" alt="Mostly Cloudy">'
+  },
+  {codeValue: "28",
+    description: "mostly cloudy",
+    imageSource: "./images/mostlyCloudy.svg",
+    imageTag: '<img src="./images/mostlyCloudy.svg" alt="Mostly Cloudy">'
+  },
+  {codeValue: "29",
+    description: "partly cloudy",
+    imageSource: "./images/partlyCloudy.svg",
+    imageTag: '<img src="./images/partlyCloudy.svg" alt="Partly Cloudy">'
+  },
+  {codeValue: "30",
+    description: "partly cloudy",
+    imageSource: "./images/partlyCloudy.svg",
+    imageTag: '<img src="./images/partlyCloudy.svg" alt="Partly Cloudy">'
+  },
+  {codeValue: "31",
+    description: "clear",
+    imageSource: "./images/clearNight.svg",
+    imageTag: '<img src="./images/clearNight.svg" alt="Clear Night">'
+  },
+  {codeValue: "32",
+    description: "sunny",
+    imageSource: "./images/sunny.svg",
+    imageTag: '<img src="./images/sunny.svg" alt="Sunny">'
+  },
+  {codeValue: "33",
+    description: "fair",
+    imageSource: "./images/fair.svg",
+    imageTag: '<img src="./images/fair.svg" alt="Fair">'
+  },
+  {codeValue: "34",
+    description: "fair",
+    imageSource: "./images/fair.svg",
+    imageTag: '<img src="./images/fair.svg" alt="Fair">'
+  },
+  {codeValue: "35",
+    description: "mixed rain and hail",
+    imageSource: "./images/rainAndHail.svg",
+    imageTag: '<img src="./images/rainAndHail.svg" alt="Rain and Hail">'
+  },
+  {codeValue: "36",
+    description: "hot",
+    imageSource: "./images/hotSunny.svg",
+    imageTag: '<img src="./images/hotSunny.svg" alt="Hot">'
+  },
+  {codeValue: "37",
+    description: "isolated thunderstorms",
+    imageSource: "./images/scatteredThunderstorm.svg",
+    imageTag: '<img src="./images/scatteredThunderstorm.svg" alt="Isolated Thunderstorm">'
+  },
+  {codeValue: "38",
+    description: "scattered thunderstorms",
+    imageSource: "./images/scatteredThunderstorm.svg",
+    imageTag: '<img src="./images/scatteredThunderstorm.svg" alt="Scattered Thunderstorm">'
+  },
+  {codeValue: "39",
+    description: "scattered thunderstorms",
+    imageSource: "./images/scatteredThunderstorm.svg",
+    imageTag: '<img src="./images/scatteredThunderstorm.svg" alt="Scattered Thunderstorm">'
+  },
+  {codeValue: "40",
+    description: "scattered showers",
+    imageSource: "./images/scatteredRain.svg",
+    imageTag: '<img src="./images/scatteredRain.svg" alt="Scattered Showers">'
+  },
+  {codeValue: "41",
+    description: "heavy snow",
+    imageSource: "./images/heavySnow.svg",
+    imageTag: '<img src="./images/heavySnow.svg" alt="Heavy Snow">'
+  },
+  {codeValue: "42",
+    description: "scattered snow showers",
+    imageSource: "./images/scatteredSnow.svg",
+    imageTag: '<img src="./images/scatteredSnow.svg" alt="Scattered Snow Showers">'
+  },
+  {codeValue: "43",
+    description: "heavy snow",
+    imageSource: "./images/heavySnow.svg",
+    imageTag: '<img src="./images/heavySnow.svg" alt="Heavy Snow">'
+  },
+  {codeValue: "44",
+    description: "partly cloudy",
+    imageSource: "./images/partlyCloudy.svg",
+    imageTag: '<img src="./images/partlyCloudy.svg" alt="Partly Cloudy">'
+  },
+  {codeValue: "45",
+    description: "thundershowers",
+    imageSource: "./images/thunderstorm.svg",
+    imageTag: '<img src="./images/thunderstorm.svg" alt="Thundershowers">'
+  },
+  {codeValue: "46",
+    description: "snow showers",
+    imageSource: "./images/snow.svg",
+    imageTag: '<img src="./images/snow.svg" alt="Snow Showers">'
+  },
+  {codeValue: "47",
+    description: "isolated thundershowers",
+    imageSource: "./images/scatteredThunderstorm.svg",
+    imageTag: '<img src="./images/scatteredThunderstorm.svg" alt="Isolated Thundershowers">'
+  },
+  {codeValue: "3200",
+    description: "not available",
+    imageSource: "",
+    imageTag: 'return "Data Not Available At This Time"'
+  }
+]
+var arraylength = Conditions.length
+var Clothes = [
+  {clothingName: "gloves",
+    tempFactor: "<= 60",
+    conditions: ["snow", "windy", "cold"],
+    imageSource: "./images/gloves.svg",
+    imageTag: '<img src="./images/gloves.svg" alt="gloves">'
+  },
+  {clothingName: "winterHat",
+    tempFactor: "<= 60",
+    conditions: ["snow", "windy", "cold"],
+    imageSource: "./images/winterHat.svg",
+    imageTag: '<img src="./images/winterHat.svg" alt="winter hat">'
+  },
+  {clothingName: "hoodie",
+    tempFactor: "<= 60",
+    conditions: ["snow", "windy", "cold"],
+    imageSource: "./images/hoodie.svg",
+    imageTag: '<img src="./images/hoodie.svg" alt="hoodie">'
+  },
+  {clothingName: "boots",
+    tempFactor: "<= 60",
+    conditions: ["snow", "rain"],
+    imageSource: "./images/boots.svg",
+    imageTag: '<img src="./images/boots.svg" alt="boots">'
+  },
+  {clothingName: "mittens",
+    tempFactor: "<= 60",
+    conditions: ["snow", "windy", "cold"],
+    imageSource: "./images/mittens.svg",
+    imageTag: '<img src="./images/mittens.svg" alt="mittens">'
+  },
+  {clothingName: "scarf",
+    tempFactor: "<= 60",
+    conditions: ["snow", "windy", "cold"],
+    imageSource: "./images/scarf.svg",
+    imageTag: '<img src="./images/scarf.svg" alt="scarf">'
+  },
+  {clothingName: "baseballHat",
+    tempFactor: "",
+    conditions: ["!==snow", "!==rain", "!==sleet"],
+    imageSource: "./images/baseballHat.svg",
+    imageTag: '<img src="./images/baseballHat.svg" alt="baseball hat">'
+  },
+  {clothingName: "rainBoots",
+    tempFactor: "",
+    conditions: ["rain", "sleet"],
+    imageSource: "./images/rainBoots.svg",
+    imageTag: '<img src="./images/rainBoots.svg" alt="rain boots">'
+  },
+  {clothingName: "tennisShoes",
+    tempFactor: "",
+    conditions: ["!==snow", "!==rain", "!==sleet"],
+    imageSource: "./images/tennisShoes.svg",
+    imageTag: '<img src="./images/tennisShoes.svg" alt="tennis shoes">'
+  },
+  {clothingName: "longPants",
+    tempFactor: "<= 70",
+    conditions: [""],
+    imageSource: "./images/longPants.svg",
+    imageTag: '<img src="./images/longPants.svg" alt="long pants">'
+  },
+  {clothingName: "longSleeveShirt",
+    tempFactor: "< 65",
+    conditions: [""],
+    imageSource: "./images/longSleeveShirt.svg",
+    imageTag: '<img src="./images/longSleeveShirt.svg" alt="long sleeve shirt">'
+  },
+  {clothingName: "shortSleeveTshirt",
+    tempFactor: ">= 65",
+    conditions: [""],
+    imageSource: "./images/shortSleeveTshirt.svg",
+    imageTag: '<img src="./images/shortSleeveTshirt.svg" alt="short sleeve Tshirt">'
+  },
+  {clothingName: "sunglasses",
+    tempFactor: "",
+    conditions: ["sunny", "partly cloudy"],
+    imageSource: "./images/sunglasses.svg",
+    imageTag: '<img src="./images/sunglasses.svg" alt="sunglasses">'
+  },
+  {clothingName: "shorts",
+    tempFactor: "> 70",
+    conditions: [""],
+    imageSource: "./images/shorts.svg",
+    imageTag: '<img src="./images/shorts.svg" alt="shorts">'
+  }
+]
+var clickEventImg = [
+  {name: "correct",
+    imageSource: "./images/greenCheck.svg",
+    imageTag: "<img scr='./images/greenCheck.svg' alt='Correct'>",
+    Response: "'That is a great idea for today!'"
+  },
+  {name: "tryAgain",
+    imageSource: "./images/redX.svg",
+    imageTag: "<img scr='./images/redX.svg' alt='Try Again'>",
+    Response: "'That is not the best choice for the weather.'"
+  }
+]
---
+++ public/js/main.js
@@ -0,0 +1,215 @@
+//clothes will be an array of objects with the following defined: temp range, season, condition,
+
+
+//document ready
+//event listener
+$(document).ready(function(){
+  loadFromLocalStorage ();
+  $('#submit').on('click', function(event){
+    event.preventDefault();
+    city = $('#city').val();
+    state = $('#state').val();
+    AJAX(city, state);
+    checkToSave()
+  })
+});
+
+function checkToSave () {
+  if ($('#checkToSave').is(':checked')) {
+    localStorage.setItem('city', $('#city').val());
+    var city = localStorage.getItem('city')
+      console.log(city)
+    localStorage.setItem('state', $('#state').val())
+    var state = localStorage.getItem('state')
+      console.log(state)
+  }
+}
+
+function loadFromLocalStorage () {
+  $('#city').val(localStorage.getItem("city"))
+  $('#state').val(localStorage.getItem("state"))
+}
+
+
+// localStorage.setItem(city, state)
+//   var city = localStorage.getItem("#city");
+//       console.log("Saving " + this.value);
+//   var state = localStorage.getItem("#state", this.value);
+//       console.log("Saving " + this.value)
+
+// function load_data() {
+//   var input = document.getElementById("city", "state");
+//   input.value = localStorage.getItem("server");
+// }
+//
+// load_data();
+
+
+function AJAX(city, state){
+  var weatherURL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + city + '%2C%20' + state +  '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+  $.get(weatherURL, function(weatherYahoo){
+    display(weatherYahoo)
+    console.log(weatherYahoo)
+  })
+}
+function display(weatherYahoo) {
+  displayTemp(weatherYahoo)
+  displaySkies(weatherYahoo)
+  displayDate(weatherYahoo)
+  correctSeason(weatherYahoo)
+  correctCondition(weatherYahoo)
+  searchDescription(weatherYahoo)
+  var copyClothes = Clothes.slice(0);
+  displayClothes1(weatherYahoo)
+  displayClothes2(weatherYahoo)
+  displayClothes3(weatherYahoo)
+  displayClothes4(weatherYahoo)
+  displayClothes5(weatherYahoo)
+  displayClothes6(weatherYahoo)
+  Clothes = copyClothes.slice(0);
+}
+
+var rnd = Math.floor(Math.random() * Clothes.length)
+
+function displayClothes1 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes1').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+function displayClothes2 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes2').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+function displayClothes3 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes3').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+function displayClothes4 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes4').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+function displayClothes5 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes5').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+function displayClothes6 () {
+  var rnd = Math.floor(Math.random() * Clothes.length);
+  $('#clothes6').html(Clothes[rnd].imageTag).addClass(Clothes[rnd].clothingName);
+  Clothes.splice(rnd,1);
+}
+
+function displayTemp(data){
+  var temperature = data.query.results.channel.item.condition.temp
+  $('#temperature').text(temperature)
+  $('#largeTemp').text(temperature)
+};
+function displaySkies(data){
+  // console.log(data);
+  var skies = data.query.results.channel.item.condition.text
+  $('#skies').text(skies)
+  return skies
+};
+
+function correctCondition (data) {
+  var imgData = data.query.results.channel.item.condition.text
+  var splitCondition = imgData.split('/');
+  console.log(splitCondition);
+  return splitCondition
+}
+console.log(Conditions[5]);
+console.log(arraylength);
+
+function searchDescription (data){
+  console.log(data)
+  for (var i=0; i<arraylength; i++) {
+    if (Conditions[i].description.toLowerCase() === displaySkies(data).toLowerCase()) {
+      console.log(Conditions[i].imageTag);
+      $('#conditionImage').html(Conditions[i].imageTag);
+      return;
+    }
+  }
+  console.log('Couldn\'t find');
+}
+
+var fullYear = (new Date().getFullYear())
+
+function getFirstDayThisSpring (){
+  var dateThisSpring = '20 March ' + fullYear + ' 00:00:00'
+  var firstThisSpring = new Date(dateThisSpring);
+  var n = firstThisSpring.valueOf();
+  return n
+};
+
+function getFirstDaySummer (){
+  var dateSummer = '20 June ' + fullYear + ' 00:00:00'
+  var firstSummer = new Date(dateSummer);
+  var n = firstSummer.valueOf();
+  return n
+};
+
+function getFirstDayFall (){
+  var dateFall = '23 September ' + fullYear
+  var firstFall = new Date(dateFall);
+  var n = firstFall.valueOf();
+  return n
+};
+
+function getFirstDayWinter (){
+  var dateWinter ='22 December ' + fullYear + ' 00:00:00'
+  var firstWinter = new Date(dateWinter);
+  var n = firstWinter.valueOf();
+  return n
+};
+
+function getFirstDayNextSpring (){
+  var nextYear = fullYear + 1
+  var dateNextSpring = '20 March ' + nextYear + ' 00:00:00'
+  var firstNextSpring = new Date(dateNextSpring);
+  var n = firstNextSpring.valueOf();
+  return n
+};
+
+
+var seasonFall = './images/fall.svg'
+var seasonWinter = './images/winter.svg'
+var seasonSummer = './images/summer.svg'
+var seasonSpring = './images/spring.svg'
+
+var seasonImg = [seasonSpring, seasonSummer, seasonFall, seasonWinter]
+
+function displayDate(data) {
+  var date = data.query.results.channel.item.condition.date
+  console.log(date);
+  $('#date').text(date)
+};
+
+
+function correctSeason (data) {
+  var d = new Date();
+  var numToday = d.valueOf();
+  var numSpring = getFirstDayThisSpring();
+  var numSummer = getFirstDaySummer();
+  var numFall = getFirstDayFall();
+  var numWinter = getFirstDayWinter();
+  console.log(numToday);
+  if (numToday >= numSpring && numToday < numSummer) {
+    console.log('Spring');
+    $('.Seasons').html('<img src="'+seasonImg[0]+'">');
+  }
+  else if (numToday >= numSummer && numToday < numFall) {
+    console.log('Summer');
+    $('.Seasons').html('<img src="'+seasonImg[1]+'">');
+  }
+  else if (numToday >= numFall && numToday < numWinter) {
+    console.log('Fall');
+    $('.Seasons').html('<img src="'+seasonImg[2]+'">');
+  }
+  else {
+    console.log('Winter');
+    $('.Seasons').html('<img src="'+seasonImg[3]+'">');
+    }
+  }
